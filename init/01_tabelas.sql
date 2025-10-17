ff08b16fedb3114de27996cd5190b2292f3094af
root <root@PSERRJO1637.grupotba.intra.corp>
2025-10-17 17:43:11 -0300

    Adicionando idade

--- init/01_tabelas.sql
+++ init/01_tabelas.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS estudantes (
     id SERIAL PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
+    idade INT NOT NULL,
     materia VARCHAR(100) NOT NULL
 );
--- init/02_dados.sql
+++ init/02_dados.sql
@@ -1,6 +1,6 @@
-INSERT INTO estudantes (nome, materia) VALUES
-('Ana Silva', 'Matemática'),
-('Bruno Souza', 'História'),
-('Carla Mendes', 'Biologia'),
-('Daniel Oliveira', 'Química'),
-('Eduarda Lima', 'Física');
+INSERT INTO estudantes (nome, idade,  materia) VALUES
+('Ana Silva', 20, 'Matemática'),
+('Bruno Souza', 22, 'Física'),
+('Carla Mendes', 19, 'Química'),
+('Daniel Oliveira', 21, 'Biologia'),
+('Eduarda Lima', 23, 'História');
